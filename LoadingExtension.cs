173b92bbb1a9193aff97f35508a335fa42d4e5cc
UncleTookie <268013950+UncleTookie@users.noreply.github.com>
2026-03-13 13:20:06 -0700

    Add files via upload

---
+++ LoadingExtension.cs
@@ -0,0 +1,21 @@
+﻿using ICities;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AssetImporterIndexDebug
+{
+    public class LoadingExtension : LoadingExtensionBase
+    {
+        private Harmony harmony;
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            Debug.Log("[AssetImporterIndexDebug] Initializing Harmony patches...");
+
+            harmony = new Harmony("com.assetimporter.indexdebug");
+            harmony.PatchAll();
+
+            Debug.Log("[AssetImporterIndexDebug] Harmony patches applied.");
+        }
+    }
+}
